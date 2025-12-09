fffd3f5ae6d5f48d9cbfe457fecb001ad971bf83
Bhoopendra Singh <bsyadav2002.apj@gmail.com>
2025-12-09 15:00:33 +0530

    some changes in main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import Routing from './Routing.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Routing />
+    <App />
   </StrictMode>
 );
